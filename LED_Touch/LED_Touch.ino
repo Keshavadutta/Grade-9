3b539d345be92720383602b4401923acedd73c7b
akhilp-boop <akhil.p@orchidsintl.edu.in>
2025-09-16 11:29:39 +0530

    Update LED_Touch.ino

--- LED_Touch/LED_Touch.ino
+++ LED_Touch/LED_Touch.ino
@@ -1,19 +1,22 @@
-#define LED_PIN 2        // LED connected to GPIO 2
-#define TOUCH_PIN 4      // Touch input connected to GPIO 4
+// Program to test the touch feature of the ESP32
+// ESP32 Touch Test - touch pin - Touch0 is T0 which is on GPIO 4
+int onboard_led = 2;
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);  // Set LED pin as output
-  Serial.begin(115200);     // Start serial communication for debugging
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("ESP32 Touch Test");
+  pinMode(onboard_led, OUTPUT);
 }
 
 void loop() {
-  int touchValue = touchRead(TOUCH_PIN);  // Read touch input on GPIO 4
-
-  if (touchValue <= 20) {  // If the touch sensor is triggered
-    digitalWrite(LED_PIN, HIGH);  // Turn on the LED
-  } else {
-    digitalWrite(LED_PIN, LOW);   // Turn off the LED
+  Serial.println(touchRead(T0)); // get value using T0
+  if (touchRead(T0) <= 20) {
+    digitalWrite(onboard_led, HIGH);
+    Serial.println("LED is activated as Touch is detected");
   }
-
-  delay(1000);  // Short delay to avoid constantly reading the sensor
+  else {
+    digitalWrite(onboard_led, LOW);
+  }
+  delay(1000); // check for touch value after every 1 second
 }
